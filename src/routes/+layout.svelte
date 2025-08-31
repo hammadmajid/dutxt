d8b79c4e4cce46e16676672a19bc9c40de197bd5
Hammad Majid <hammadmajid@proton.me>
2025-08-31 16:48:47 +0500

    feat: integrate Terminal.css and enhance layout
    - Added `terminal.css` for styling and updated dependencies.
    - Improved main layout with a terminal-inspired navigation bar and container.
    - Refined structure by removing redundant footer and navigation from the page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import favicon from '$lib/assets/favicon.svg';
+    import 'terminal.css'
 
     let {children} = $props();
 </script>
@@ -8,4 +9,20 @@
     <link rel="icon" href={favicon}/>
 </svelte:head>
 
-{@render children?.()}
+<div class="container-fluid">
+    <nav class="terminal-nav">
+        <header class="terminal-logo">
+            <span class="terminal-prompt">Dutxt</span>
+        </header>
+        <div class="terminal-menu">
+            <ul>
+                <li><a href="https://github.com/hammadmajid/dutxt" target="_blank">Source</a></li>
+                <li><a href="https://hammadmajid.pages.dev" target="_blank">Author</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div>
+        {@render children?.()}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,4 @@
     import Editor from "$lib/components/Editor.svelte";
 </script>
 
-<nav>
-    <h1>Dutext</h1>
-</nav>
-
 <Editor language="cpp"/>
-
-<footer>
-    <p>Made by <a href="https://github.com/hammadmajid">Hammad Majid</a></p>
-</footer>
